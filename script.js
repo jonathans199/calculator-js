7ba29724f8de7fea7085ec435a0bdc01a1ca9f59
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-08-08 09:11:16 -0400

    html markup and some js

---
+++ script.js
@@ -0,0 +1,23 @@
+let 
+num1 = 1, 
+num2 = 2;
+
+
+
+result = num1 + num2
+
+console.log(result)
+
+
+// function num1Function() {
+//   let result = (document.getElementById('inputField'))
+
+//     result.value = 1
+// }
+
+// function num2Function() {
+//   let result = (document.getElementById('inputField'))
+
+//     result.value = 2
+// }
+
