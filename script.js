cc98a519aa556abac224d3b594e30ca2f4dc90fc
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-08-12 21:57:25 -0400

    was able to get a multi digit number saving to a variable

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 
-let display = (document.getElementById('display'))
-let number1 = null
-let number2 = null
-let total = null
+/* 
+CALCULATOR 
+  1 - get value when btn is pressed
+  2 - display that value in the display 
+  3 - if btn pressed more than once keep adding btn to string
+  4 - once an operator btn is pressed
+    - 1 - convert that string to int
+    - 2 - get the second number 
+    - 3 - do the calculation
+  5 - display the result onto display
+
+  - put all values in an array
+  - get all the items out together contact
+  - 
+*/
+
+let display = document.getElementById('display')
+let num1 = null
+let arrayNum1 = []
+let arrayNum2 = []
 
 function setValue(btnValue){
-      number1 = btnValue.getAttribute('data-btn-number')
-      display.value = number1
-  console.log(number1)
+
+        //gets number from num btn pressed
+      number = btnValue.getAttribute('data-btn-number');
+
+        //push number from value into array => array
+      arrayNum1.push(number);
+
+        //assign first number to num1
+      num1 = (display.value = arrayNum1.join(''));
+  console.log(num1)
 }
 
+function addition(){
 
+  //clears display
+  // display.reset
 
+  // add num1 + num2
+  // result = num1 + num2
+  // console.log(result)
+
+  // number = btnValue.getAttribute('data-btn-number');
+
+  // arrayNum2.push(numbeer)
+  
+  // operator.getAttribute('data-btn-operation')
+}
 
 // if (document.getElementById('btn1').click() ){
 //   inputField.value = 1
@@ -29,12 +65,6 @@ function setValue(btnValue){
 //   let inputField = (document.getElementById('inputField')).value = 2
 //   num2 = 2
 // }
-
-
-
-
-
-
 
 // // dresult = num1 + num2 
 
