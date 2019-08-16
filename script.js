6c11be0ae8d99a0e6d7982cea8761c843a43da0d
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-08-16 10:00:08 -0400

    updated js and html ;

--- script.js
+++ script.js
@@ -15,28 +15,176 @@ CALCULATOR
   - 
 */
 
-let display = document.getElementById('display')
+// let display = document.getElementById('display')
+// let num1 = null
+// let array1 = []
+
+// let btn = document.querySelector('button')
+
+// console.log(document.querySelectorAll('#btn-list'))
+
+// let btnItem = (document.querySelectorAll('#btn-list > button'))
+
+// for(var i = 0; i < btnItem.lenght; i++){
+//   btnItem[i].addEventListener("click", function(){
+
+//     num1 = +this.value;
+//     console.log (num1.value)
+//   })
+// }
+
+// console.log(btnItem)
+/*----------------- option 1 # ---------------*/
+
+// var test =  document.querySelectorAll('.btn-number');
+
+// // gets all the buttons elements
+// test.forEach(function(userItem){
+//   console.log(userItem.value) 
+// });
+
+/*------------------- end -------------------*/
+
+
+
+/*----------------- option 2 - WORKING up to working clearing display ---------------*/
+
+// let display = document.getElementById('display')
+// let num1 = null
+// let num2 = null
+
+// let array = []
+
+// function setValue(btnValue) {
+
+//     //gets number from num btn pressed
+//   number = btnValue.getAttribute('data-btn-number');
+
+//     //push number from value into array => array
+//   array.push(number);
+
+//     //assign first number to num1
+//   num1 = (display.value = array.join(''));
+//   console.log(num1)
+// }
+
+// function addition(){
+//   display.value = 0
+//   console.log(num1)
+// }
+
+/*----------------- option 2.b - WORKING  ---------------*/
+
+const display = document.getElementById('display')
+let number = null
+let array = []
+
+let turn1 = false
+
 let num1 = null
-let arrayNum1 = []
-let arrayNum2 = []
+let num2 = null
+let total = null
 
-function setValue(btnValue){
 
-        //gets number from num btn pressed
-      number = btnValue.getAttribute('data-btn-number');
+function setValue(btnValue) {
 
-        //push number from value into array => array
-      arrayNum1.push(number);
+  num = btnValue.getAttribute('data-btn-number'); //gets number from num btn pressed
 
-        //assign first number to num1
-      num1 = (display.value = arrayNum1.join(''));
-  console.log(num1)
+  array.push(num); //push number from value into array => array
+
+  number = (display.value = array.join(''));  //assign first number to number
+  console.log(number)
+
+  turn1 = true
 }
 
-function addition(){
+function addition() {
+
+  if (number > 0 && turn1 == !false) {
+    num1 = number  //let num1 be number
+  }
+
+  display.value = 0  //set display to 0  
+  array = [] //empty the array 
+
+  console.log('array & display cleared this is number1 ' + num1)
+
+}
+
+
+  // get total
+// total = num1 + num2
+
+
+/*----------------- option #3 - Get value into array - FAILED ---------------*/
+
+// function setValue(number){
+//   num1 = number
+//   console.log(num1)
+// }
+
+// function setValue(x){
+//   console.log(x)
+
+//         //gets number from num btn pressed
+//       // number = getAttribute('value')
+//       // console.log(x)
+
+//         //push number from value into array => array
+//       // array1.push(number);
+
+//         //assign first number to num1
+//       // num1 = (display.value = array1.join(''));
+// }
+
+
+/*------------------- end -------------------*/
+
+
+
+/*----------------- option #4 ---------------*/
+// var num = null;
+// var ele = document.querySelectorAll(".btn-list > button.btn-number");
+// console.log(ele)
+
+// for (var i = 0; i < ele.length; i++){
+//   ele[i].addEventListener("click", function(){
+//     num = +this.innerHTML;
+//     console.log(num);
+//   })
+// }
+
+// from here https://stackoverflow.com/questions/10941606/how-to-get-the-value-of-selected-button-in-twitter-bootstrap-button-group
+
+/*------------------- end -------------------*/
+
+
+
+
+// btn.onclick = function(){
+
+//   console.log(btn.value)
+
+//   // array1.push(btn.value)
+
+// }
+
+
+
+// console.log(display)
+
+// btn.onclick = function(){
+
+// }
+
+
+
+
+
+// function addition(){
 
   //clears display
-  display.reset();
+  // display = 0;
 
   // add num1 + num2
   // result = num1 + num2
@@ -46,7 +194,7 @@ function addition(){
 
   // arrayNum2.push(numbeer)
   // operator.getAttribute('data-btn-operation')
-}
+// }
 
 // if (document.getElementById('btn1').click() ){
 //   inputField.value = 1
@@ -79,13 +227,5 @@ function addition(){
 
 // //     result.value = 2
 // // }
-
-
-
-
-
-
-
-
 
 
